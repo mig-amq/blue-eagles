a8510fa8fd994a1c5fe9f629fb109ea31a859d92
Earon <jonearon8@gmail.com>
2018-11-19 16:49:05 +0800

    Beta 0.4
    Additional Fixes + Instructions on temporary run

---
+++ trials-experiments/public/10-M/data.js
@@ -0,0 +1,198 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-a1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 0.023895741685205252,
+        "pitch": 0.154033494060009,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1478796998492271,
+          "pitch": 0.2818346124124993,
+          "rotation": 0,
+          "target": "4-next-floor"
+        },
+        {
+          "yaw": 3.1320864856801958,
+          "pitch": 0.4667120123998192,
+          "rotation": 0,
+          "target": "3-b2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-a2",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 2.990697768352434,
+        "pitch": -0.05251151162989842,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1478796998492271,
+          "pitch": 0.2818346124124993,
+          "rotation": 0,
+          "target": "4-next-floor"
+        },
+        {
+          "yaw": 3.1320864856801958,
+          "pitch": 0.4667120123998192,
+          "rotation": 0,
+          "target": "3-b2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-b1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 3.0175738959805507,
+        "pitch": 0.2422899571695396,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5702178401403,
+          "pitch": 0.18028129277830374,
+          "rotation": 4.71238898038469,
+          "target": "4-next-floor"
+        },
+        {
+          "yaw": -3.0979518389225422,
+          "pitch": 0.3368888776082386,
+          "rotation": 0,
+          "target": "0-a1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-b2",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 1.200445000826324,
+        "pitch": 0.14859695510786253,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5702178401403,
+          "pitch": 0.18028129277830374,
+          "rotation": 4.71238898038469,
+          "target": "4-next-floor"
+        },
+        {
+          "yaw": -3.0979518389225422,
+          "pitch": 0.3368888776082386,
+          "rotation": 0,
+          "target": "0-a1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-next-floor",
+      "name": "next-floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "M10",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ trials-experiments/public/12/data.js
@@ -0,0 +1,684 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-a1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": -0.03375705752682201,
+        "pitch": 0.13136919108959688,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.138969484056819,
+          "pitch": 0.36920123623396783,
+          "rotation": 0,
+          "target": "12-next-floor"
+        },
+        {
+          "yaw": 2.1353261857764156,
+          "pitch": 0.37027031480931427,
+          "rotation": 0.7853981633974483,
+          "target": "11-t1"
+        },
+        {
+          "yaw": -2.2984142251314523,
+          "pitch": 0.27473390912604856,
+          "rotation": 5.497787143782138,
+          "target": "11-t1"
+        },
+        {
+          "yaw": -1.714907408330685,
+          "pitch": 0.28369923501601235,
+          "rotation": 0,
+          "target": "2-b1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.083446667351014,
+          "pitch": -0.08696993508317696,
+          "title": "Restrooms",
+          "text": "&lt;edit text&gt;"
+        },
+        {
+          "yaw": -2.139913663784627,
+          "pitch": -0.15025833297853808,
+          "title": "Info Popup",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-a2",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 1.589901044024514,
+        "pitch": 0.25717763673523564,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.138969484056819,
+          "pitch": 0.36920123623396783,
+          "rotation": 0,
+          "target": "12-next-floor"
+        },
+        {
+          "yaw": 2.1353261857764156,
+          "pitch": 0.37027031480931427,
+          "rotation": 0.7853981633974483,
+          "target": "11-t1"
+        },
+        {
+          "yaw": -2.2984142251314523,
+          "pitch": 0.27473390912604856,
+          "rotation": 5.497787143782138,
+          "target": "11-t1"
+        },
+        {
+          "yaw": -1.714907408330685,
+          "pitch": 0.28369923501601235,
+          "rotation": 0,
+          "target": "2-b1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.083446667351014,
+          "pitch": -0.08696993508317696,
+          "title": "Restrooms",
+          "text": "&lt;edit text&gt;"
+        },
+        {
+          "yaw": -2.139913663784627,
+          "pitch": -0.15025833297853808,
+          "title": "Info Popup",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-b1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": -1.5485425509535435,
+        "pitch": 0.3033271053741391,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9977586639476606,
+          "pitch": 0.22665581726916173,
+          "rotation": 0,
+          "target": "5-d1"
+        },
+        {
+          "yaw": 1.586631286992807,
+          "pitch": 0.35916019143295586,
+          "rotation": 0,
+          "target": "1-a2"
+        },
+        {
+          "yaw": 0.4464260552524948,
+          "pitch": 0.3527077560813119,
+          "rotation": 0.7853981633974483,
+          "target": "4-c1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.537803970912929,
+          "pitch": -0.08171086553145557,
+          "title": "Help Desk",
+          "text": "&lt;edit text&gt;"
+        },
+        {
+          "yaw": -3.0660556380126103,
+          "pitch": -0.07063189274555981,
+          "title": "Archives Area",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "3-b2",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 1.7270651110163628,
+        "pitch": 0.3466757987486808,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9977586639476606,
+          "pitch": 0.22665581726916173,
+          "rotation": 0,
+          "target": "5-d1"
+        },
+        {
+          "yaw": 1.586631286992807,
+          "pitch": 0.35916019143295586,
+          "rotation": 0,
+          "target": "1-a2"
+        },
+        {
+          "yaw": 0.4464260552524948,
+          "pitch": 0.3527077560813119,
+          "rotation": 0.7853981633974483,
+          "target": "4-c1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.537803970912929,
+          "pitch": -0.08171086553145557,
+          "title": "Help Desk",
+          "text": "&lt;edit text&gt;"
+        },
+        {
+          "yaw": -3.0660556380126103,
+          "pitch": -0.07063189274555981,
+          "title": "Archives Area",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "4-c1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": -3.134966928949602,
+        "pitch": -0.16093548634568577,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1901520438993103,
+          "pitch": 0.4575331699805947,
+          "rotation": 0,
+          "target": "2-b1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1230443311864953,
+          "pitch": -0.026071446495073758,
+          "title": "Student Space",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "5-d1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": -2.247115486279162,
+        "pitch": -0.055478569067748396,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.331703519545954,
+          "pitch": 0.2056409744002039,
+          "rotation": 0,
+          "target": "10-s1"
+        },
+        {
+          "yaw": 3.1054689720260775,
+          "pitch": 0.13224384743922002,
+          "rotation": 6.283185307179586,
+          "target": "7-e1"
+        },
+        {
+          "yaw": 1.364028400077153,
+          "pitch": 0.4158490176677354,
+          "rotation": 0,
+          "target": "3-b2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.130368865343261,
+          "pitch": -0.25608433471991887,
+          "title": "Restrooms",
+          "text": ""
+        },
+        {
+          "yaw": -1.7874740596379084,
+          "pitch": -0.07767765508029001,
+          "title": "Drinking Fountain",
+          "text": ""
+        },
+        {
+          "yaw": 2.015621880310566,
+          "pitch": 0.11294237544582941,
+          "title": "Help Desk",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "6-d2",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 1.152230015440967,
+        "pitch": 0.4025166737096235,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.331703519545954,
+          "pitch": 0.2056409744002039,
+          "rotation": 0,
+          "target": "10-s1"
+        },
+        {
+          "yaw": 3.1054689720260775,
+          "pitch": 0.13224384743922002,
+          "rotation": 6.283185307179586,
+          "target": "7-e1"
+        },
+        {
+          "yaw": 1.364028400077153,
+          "pitch": 0.4158490176677354,
+          "rotation": 0,
+          "target": "3-b2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.130368865343261,
+          "pitch": -0.25608433471991887,
+          "title": "Restrooms",
+          "text": ""
+        },
+        {
+          "yaw": -1.7874740596379084,
+          "pitch": -0.07767765508029001,
+          "title": "Drinking Fountain",
+          "text": ""
+        },
+        {
+          "yaw": 2.015621880310566,
+          "pitch": 0.11294237544582941,
+          "title": "Help Desk",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "7-e1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": -3.1342602093281418,
+        "pitch": 0.01744357631408988,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1123064269582237,
+          "pitch": 0.15767906031163648,
+          "rotation": 0,
+          "target": "9-f1"
+        },
+        {
+          "yaw": 0.7860731976827289,
+          "pitch": 0.5592076186745487,
+          "rotation": 0,
+          "target": "6-d2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1740465153704065,
+          "pitch": 0.06056311855295604,
+          "title": "Computer Terminals",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "8-e2",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 0.24755180206858363,
+        "pitch": 0.03703412333056377,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1123064269582237,
+          "pitch": 0.15767906031163648,
+          "rotation": 0,
+          "target": "9-f1"
+        },
+        {
+          "yaw": 0.7860731976827289,
+          "pitch": 0.5592076186745487,
+          "rotation": 0,
+          "target": "6-d2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1740465153704065,
+          "pitch": 0.06056311855295604,
+          "title": "Computer Terminals",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "9-f1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 0.19137121239796429,
+        "pitch": 0.3124862819673062,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.07478314213318,
+          "pitch": 0.13735649946336004,
+          "rotation": 0,
+          "target": "8-e2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7225594235630055,
+          "pitch": -0.03453237844561485,
+          "title": "Archives Area",
+          "text": "&lt;edit text&gt;"
+        },
+        {
+          "yaw": -0.7275869097014525,
+          "pitch": 0.04791001352596069,
+          "title": "Area Under Maintenance",
+          "text": "&lt;edit text&gt;"
+        },
+        {
+          "yaw": 1.7921336234876,
+          "pitch": -0.000897292698228469,
+          "title": "Student Space",
+          "text": "&lt;edit text&gt;"
+        }
+      ]
+    },
+    {
+      "id": "10-s1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 2.4197715080404745,
+        "pitch": 0.20518282621553396,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-t1",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.134580928290408,
+          "pitch": 0.2956516829302771,
+          "rotation": 0,
+          "target": "12-next-floor"
+        },
+        {
+          "yaw": -2.194285389661445,
+          "pitch": 0.21180189145075623,
+          "rotation": 5.497787143782138,
+          "target": "0-a1"
+        },
+        {
+          "yaw": 2.1246818323717056,
+          "pitch": 0.4062912089684918,
+          "rotation": 0.7853981633974483,
+          "target": "0-a1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5802076310290385,
+          "pitch": -0.019098417088731168,
+          "title": "Area Under Maintenance",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "12-next-floor",
+      "name": "next-floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "12",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
