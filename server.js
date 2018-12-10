bc5969b2957bde21cae6646b5708740073e9b134
Earon <jonearon8@gmail.com>
2018-12-11 03:00:19 +0800

    Pre-Release 0.7.1
    Heroku deployment settings

--- server.js
+++ server.js
@@ -13,5 +13,7 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "render.html"));
 })
 
-/* Listen */
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
